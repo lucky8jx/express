9422a623a5ce35a6766be452aed44d4d12c1d9a3
LiuYuebin <693150256@qq.com>
2013-11-26 13:40:00 +0800

    add a form of login

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,64 @@
 (function($) {
-	$('#logInForm').hide();
-	$('.logIn').on("click", function(e) {
-		e.preventDefault();
-		$('#logInForm').slideToggle(500);
+	// APP
+	// Global Namespace Object
+	var APP = APP || {};
+
+	// General Purpose Namespace Function
+	APP.namespace = function(ns_string) {
+		var parts = ns_string.split('.'),
+			parent = APP;
+
+		// strip redundant leading global
+		if (parts[0] === "APP") {
+			parts = parts.slice(1);
+		}
+
+		for (var i = 0, max = parts.length; i < max; i += 1) {
+			// create a property if it doesn't exist
+			if (typeof parent[parts[i]] === "undefined") {
+				parent[parts[i]] = {};
+			}
+			parent = parent[parts[i]];
+		}
+		return parent;
+	};
+
+	// Add a method() to the Function
+	if (typeof Function.prototype.method !== "function") {
+		Function.prototype.method = function (name, implementation) {
+			this.prototype[name] = implementation;
+			return this;
+		};
+	}
+
+	// LogIn form
+	APP.namespace('logIn');
+	APP.logIn = (function() {
+		var logInForm = $('#logInForm').hide(),
+			config = {
+				effect: 'slideToggle',
+				speed: 300
+			};
+
+		// init function
+		function init(conf) {
+			$.extend(config, conf);
+			$('.logIn').on('click', function(e) {
+				e.preventDefault();
+				show();
+			});
+		}
+		// show 
+		function show() {
+			logInForm.stop()[config.effect](config.speed);
+		}
+		return {
+			// private method(init) as public one
+			init: init
+		};
+	}());
+	APP.logIn.init({
+		effect: 'fadeToggle',
+		speed: 500
 	});
 })(jQuery);
